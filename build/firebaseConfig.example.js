143fa80e1178bb5451d2078f430c44b77ac70a8c
soltune <k@psycube.com>
2020-02-24 22:06:19 +0900

    added images for README.md

--- build/firebaseConfig.example.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "API_KEY",
-  authDomain: "AUTH_DOMAIN",
-  databaseURL: "DATABASE_URL",
-  projectId: "PROJECT_ID",
-  storageBucket: "STORAGE_BUCKET",
-  messagingSenderId: "MESSAGING_SENDER_ID"
-};
-
